4e8c981b2a911b2585311fb1f440aa92c5a03444
François <yyperite@hotmail.fr>
2019-07-29 09:24:27 +0200

    avancer de la calculatrice

--- calc.js
+++ calc.js
@@ -1,36 +1,56 @@
 let tab = document.getElementsByClassName("button");
 let ecran = document.getElementById("affichage");
 let egal = document.getElementById("result");
+let reset = document.getElementById("reset");
+let returner = document.getElementById("return");
+
 
 for(let i = 0; i < tab.length; i++){
 
     tab[i].addEventListener("click", affich);
-
 }
+
 function affich(){
 
-    ecran.textContent = this.textContent;
+    if(ecran.textContent == 0){
+        ecran.textContent = "";
+    }
+    ecran.textContent += this.textContent;
+    return;
+}
+function resultats(a){
+
+    ecran.textContent = eval(ecran.textContent);
+    
+    return(a);
+}
+egal.addEventListener("click", resultats);
+
+function effac(){
+
+    ecran.textContent = "0";
+
+    return;
+}
+reset.addEventListener("click", effac);
+
+function arriere(){
+
+    /*let longueur = ecran.textContent.length;
+
+    longueur -= 1;
+    ecran.textContent = longueur;*/
+
+    ecran.textContent = ecran.textContent.replace(ecran.textContent.slice(-1), "")
     return;
 }
 
-egal == eval();
- 
-egal.addEventListener("click", eval);
+returner.addEventListener("click", arriere);
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-    
 
 
 
